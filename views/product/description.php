9c60e0e384eb8f4143428b081dc5c7b36ab18d01
Saymecode <Filippov-f@mail.ru>
2016-12-14 00:33:54 +0200

    description page fix

--- views/product/description.php
+++ views/product/description.php
@@ -8,151 +8,21 @@ ProductAsset::register($this);
 
 /* @var $product Product */
 ?>
-
-<!--content-->
-<!---->
-<div class="product">
-    <div class="container">
-        <div class="col-md-3 product-price">
-
-            <div class=" rsidebar span_1_of_left">
-                <div class="of-left">
-                    <h3 class="cate">Categories</h3>
-                </div>
-                <ul class="menu">
-                    <li class="item1"><a href="#">Men </a>
-                        <ul class="cute">
-                            <li class="subitem1"><a href="#">Cute Kittens </a></li>
-                            <li class="subitem2"><a href="#">Strange Stuff </a></li>
-                            <li class="subitem3"><a href="#">Automatic Fails </a></li>
-                        </ul>
-                    </li>
-                    <li class="item2"><a href="#">Women </a>
-                        <ul class="cute">
-                            <li class="subitem1"><a href="#">Cute Kittens </a></li>
-                            <li class="subitem2"><a href="#">Strange Stuff </a></li>
-                            <li class="subitem3"><a href="#">Automatic Fails </a></li>
-                        </ul>
-                    </li>
-                    <li class="item3"><a href="#">Kids</a>
-                        <ul class="cute">
-                            <li class="subitem1"><a href="#">Cute Kittens </a></li>
-                            <li class="subitem2"><a href="#">Strange Stuff </a></li>
-                            <li class="subitem3"><a href="#">Automatic Fails</a></li>
-                        </ul>
-                    </li>
-                    <li class="item4"><a href="#">Accesories</a>
-                        <ul class="cute">
-                            <li class="subitem1"><a href="#">Cute Kittens </a></li>
-                            <li class="subitem2"><a href="#">Strange Stuff </a></li>
-                            <li class="subitem3"><a href="#">Automatic Fails</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="item4"><a href="#">Shoes</a>
-                        <ul class="cute">
-                            <li class="subitem1"><a href="#">Cute Kittens </a></li>
-                            <li class="subitem2"><a href="#">Strange Stuff </a></li>
-                            <li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!--initiate accordion-->
-
-            <!---->
-            <div class="product-middle">
-
-                <div class="fit-top">
-                    <h6 class="shop-top">Lorem Ipsum</h6>
-                    <a href="#" class="shop-now">SHOP NOW</a>
-                    <div class="clearfix"> </div>
-                </div>
-            </div>
-            <div class="sellers">
-                <div class="of-left-in">
-                    <h3 class="tag">Tags</h3>
-                </div>
-                <div class="tags">
-                    <ul>
-                        <li><a href="#">design</a></li>
-                        <li><a href="#">fashion</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">dress</a></li>
-                        <li><a href="#">fashion</a></li>
-                        <li><a href="#">dress</a></li>
-                        <li><a href="#">design</a></li>
-                        <li><a href="#">dress</a></li>
-                        <li><a href="#">design</a></li>
-                        <li><a href="#">fashion</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">dress</a></li>
-
-                        <div class="clearfix"> </div>
-                    </ul>
-
-                </div>
-
-            </div>
-            <!---->
-            <div class="product-bottom">
-                <div class="of-left-in">
-                    <h3 class="best">Best Sellers</h3>
-                </div>
-                <div class="product-go">
-                    <div class=" fashion-grid">
-                        <a href="#"><img class="img-responsive " src="images/p1.jpg" alt=""></a>
-
-                    </div>
-                    <div class=" fashion-grid1">
-                        <h6 class="best2"><a href="#" >Lorem ipsum dolor sit
-                                amet consectetuer  </a></h6>
-
-                        <span class=" price-in1"> $40.00</span>
-                    </div>
-
-                    <div class="clearfix"> </div>
-                </div>
-                <div class="product-go">
-                    <div class=" fashion-grid">
-                        <a href="#"><img class="img-responsive " src="images/p2.jpg" alt=""></a>
-
-                    </div>
-                    <div class="fashion-grid1">
-                        <h6 class="best2"><a href="#" >Lorem ipsum dolor sit
-                                amet consectetuer </a></h6>
-
-                        <span class=" price-in1"> $40.00</span>
-                    </div>
-
-                    <div class="clearfix"> </div>
-                </div>
-
-            </div>
-            <div class=" per1">
-                <a href="#" ><img class="img-responsive" src="images/pro.jpg" alt="">
-                    <div class="six1">
-                        <h4>DISCOUNT</h4>
-                        <p>Up to</p>
-                        <span>60%</span>
-                    </div></a>
-            </div>
-        </div>
-        <div class="col-md-9 product-price1">
+        <div class="col-md-10 product-price1">
             <div class="col-md-5 single-top">
                 <div class="flexslider">
                     <ul class="slides">
-                        <li data-thumb="images/<?=$imgs[0]?>.jpg">
-                            <img src="images/<?=$imgs[0]?>.jpg" />
+                        <li data-thumb="<?=Url::to("@web/images/$imgs[0].jpg")?>">
+                            <?= Html::img("@web/images/$imgs[0].jpg");?>
                         </li>
-                        <li data-thumb="images/<?=$imgs[1]?>.jpg">
-                            <img src="images/<?=$imgs[1]?>.jpg" />
+                        <li data-thumb="<?=Url::to("@web/images/$imgs[1].jpg")?>">
+                            <?= Html::img("@web/images/$imgs[1].jpg");?>
                         </li>
-                        <li data-thumb="images/<?=$imgs[2]?>.jpg">
-                            <img src="images/<?=$imgs[2]?>.jpg" />
+                        <li data-thumb="<?=Url::to("@web/images/$imgs[2].jpg")?>">
+                            <?= Html::img("@web/images/$imgs[2].jpg");?>
                         </li>
-                        <li data-thumb="images/<?=$imgs[3]?>.jpg">
-                            <img src="images/<?=$imgs[3]?>.jpg" />
+                        <li data-thumb="<?=Url::to("@web/images/$imgs[3].jpg")?>">
+                            <?= Html::img("@web/images/$imgs[3].jpg");?>
                         </li>
                     </ul>
                 </div>
@@ -228,7 +98,7 @@ ProductAsset::register($this);
                     <li data-content="television" class="selected">
                         <div class="comments-top-top">
                             <div class="top-comment-left">
-                                <img class="img-responsive" src="images/co.png" alt="">
+                                <?=Html::img('@web/images/co.png', ['class'=>"img-responsive"])?>
                             </div>
                             <div class="top-comment-right">
                                 <h6><a href="#">Hendri</a> - September 3, 2014</h6>
@@ -248,40 +118,6 @@ ProductAsset::register($this);
                     </li>
                     <div class="clearfix"></div>
                 </ul>
-            </div>
-            <div class=" bottom-product">
-                <div class="col-md-4 bottom-cd simpleCart_shelfItem">
-                    <div class="product-at ">
-                        <a href="#"><img class="img-responsive" src="images/pi3.jpg" alt="">
-                            <div class="pro-grid">
-                                <span class="buy-in">Buy Now</span>
-                            </div>
-                        </a>
-                    </div>
-                    <p class="tun">It is a long established fact that a reader</p>
-                    <a href="#" class="item_add"><p class="number item_price"><i> </i>$500.00</p></a>
-                </div>
-                <div class="col-md-4 bottom-cd simpleCart_shelfItem">
-                    <div class="product-at ">
-                        <a href="#"><img class="img-responsive" src="images/pi1.jpg" alt="">
-                            <div class="pro-grid">
-                                <span class="buy-in">Buy Now</span>
-                            </div>
-                        </a>
-                    </div>
-                    <p class="tun">It is a long established fact that a reader</p>
-                    <a href="#" class="item_add"><p class="number item_price"><i> </i>$500.00</p></a>					</div>
-                <div class="col-md-4 bottom-cd simpleCart_shelfItem">
-                    <div class="product-at ">
-                        <a href="#"><img class="img-responsive" src="images/pi4.jpg" alt="">
-                            <div class="pro-grid">
-                                <span class="buy-in">Buy Now</span>
-                            </div>
-                        </a>
-                    </div>
-                    <p class="tun">It is a long established fact that a reader</p>
-                    <a href="#" class="item_add"><p class="number item_price"><i> </i>$500.00</p></a>					</div>
-                <div class="clearfix"> </div>
             </div>
         </div>
 
