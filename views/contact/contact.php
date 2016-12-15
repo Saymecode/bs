b95e7aef97e5afcb82f4e14f6819a8a4634cf241
Saymecode <Filippov-f@mail.ru>
2016-12-15 14:38:54 +0200

    ...

--- views/contact/contact.php
+++ views/contact/contact.php
@@ -47,7 +47,7 @@ use yii\helpers\Html;
             <div class="clearfix"> </div>
         </div>
         <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5692.364723058121!2d26.98203648946602!3d49.42916208546603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xab6b194762cd15e9!2sKinoteatr+%22Planeta%22!5e0!3m2!1sit!2sit!4v1481805159249" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>        </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83069.06104484576!2d26.92521842600604!3d49.4106424906158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4732064344bb178b%3A0xd9f30b3b24d9c964!2sChmel&#39;nyc&#39;kyj%2C+Khmel&#39;nyts&#39;kyi%2C+Ucraina!5e0!3m2!1sit!2sit!4v1481805466490" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 
 </div>
